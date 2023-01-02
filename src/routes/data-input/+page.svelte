94f795ef70ee6c668662788bc842edceef7c2ff2
Martijn <martijnschermers.dev@gmail.com>
2023-01-02 16:05:13 +0100

    add: showcase page for layout and data-input components

---
+++ src/routes/data-input/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Select, Checkbox, Radio, Range } from '$lib';
+
+	let options: string[] = ['Choose a option', 'option1', 'option2', 'options3'];
+</script>
+
+<!-- Tested -->
+<Select disabled={true} bordered={true} ghost={false} {options}>
+	<p slot="label-1">Label 1</p>
+	<p slot="label-2">Label 2</p>
+	<p slot="label-3">Label 3</p>
+	<p slot="label-4">Label 4</p>
+</Select>
+
+<Checkbox indeterminate={true} color="primary" size="lg" />
+
+<Radio />
+
+<Range value={10} step={10} steps={true} />
---
+++ src/routes/layout/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Indicator from '$lib/components/layout/Indicator.svelte';
+</script>
+
+<Indicator color="primary">
+    <div slot="badge">Required</div>
+	<input type="text" placeholder="Your email address" class="input input-bordered" />
+</Indicator>
