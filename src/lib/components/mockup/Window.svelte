a3ac90b883a27e1a58a6b105c702e6ba8569f0b3
Martijn <martijnschermers.dev@gmail.com>
2023-01-05 21:23:22 +0100

    add: color prop

--- src/lib/components/mockup/Window.svelte
+++ src/lib/components/mockup/Window.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-	import type { Color, ColorStrings } from '$lib/enums/color.enum';
-
-	export let color: Color | ColorStrings = '';
 	export let bordered: boolean = true;
 </script>
 
-<div class="mockup-window bg-{color}" class:border={bordered} class:bordered-base-300={bordered}>
+<div class="mockup-window {$$props.class ? $$props.class : ''}" class:border={bordered} class:bordered-base-300={bordered}>
 	<div class="flex justify-center px-4 py-16 bordered-t" class:bordered-base-300={bordered}>
 		<slot />
 	</div>
