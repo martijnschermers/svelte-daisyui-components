f196daa4ec363ad8746fbca3e41b92696b4ee0ee
Martijn <martijnschermers.dev@gmail.com>
2023-01-06 15:33:56 +0100

    update: ring classes working properly

--- src/lib/components/data-display/Avatar.svelte
+++ src/lib/components/data-display/Avatar.svelte
@@ -2,23 +2,14 @@
 	import { Color, type ColorStrings } from '$lib/enums/color.enum';
 
 	export let ring: Color | ColorStrings = '';
+	export let rounded: boolean = false;
 	export let online: boolean = false;
 	export let offline: boolean = false;
 </script>
 
 <div
-	class="avatar rounded-full {$$props.class ? $$props.class : ''}"
+	class="avatar {$$props.class ? $$props.class : ''}"
 	class:placeholder={$$slots.placeholder}
-	class:ring-offset-base-100={ring}
-	class:ring-offset-2={ring}
-	class:ring-primary={ring == Color.Primary}
-	class:ring-secondary={ring == Color.Secondary}
-	class:ring-accent={ring == Color.Accent}
-	class:ring-info={ring == Color.Info}
-	class:ring-success={ring == Color.Success}
-	class:ring-warning={ring == Color.Warning}
-	class:ring-error={ring == Color.Error}
-	class:ring
 	class:online
 	class:offline
 >
@@ -27,7 +18,20 @@
 			<span class="text-3xl"><slot name="placeholder" /></span>
 		</div>
 	{:else}
-		<div class="w-24 rounded">
+		<div
+			class="w-24 rounded"
+			class:rounded-full={rounded}
+			class:ring-offset-base-100={ring}
+			class:ring-offset-2={ring}
+			class:ring-primary={ring == Color.Primary}
+			class:ring-secondary={ring == Color.Secondary}
+			class:ring-accent={ring == Color.Accent}
+			class:ring-info={ring == Color.Info}
+			class:ring-success={ring == Color.Success}
+			class:ring-warning={ring == Color.Warning}
+			class:ring-error={ring == Color.Error}
+			class:ring
+		>
 			<slot />
 		</div>
 	{/if}
