2aeeede12d3e573dd01090820efb25379ff64f5e
Martijn <martijnschermers.dev@gmail.com>
2023-01-06 15:31:23 +0100

    add: Submenu component

---
+++ src/lib/components/navigation/Submenu.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import ListItem from '$lib/components/navigation/ListItem.svelte';
+	import type { LinkType } from '$lib/interfaces/link.interface';
+
+	export let items: LinkType[] = [];
+</script>
+
+<li tabindex="0">
+	<span><slot name="label" /></span>
+	<ul class="bg-base-100">
+		{#each items as item}
+			<ListItem href={item.href}>{item.text}</ListItem>
+		{:else}
+			<slot />
+		{/each}
+	</ul>
+</li>
