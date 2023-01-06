04f12bed667df049d8ce1fe7f1c9128817b8d47e
Martijn <martijnschermers.dev@gmail.com>
2023-01-06 15:31:08 +0100

    add: AvatarGroup component

---
+++ src/lib/components/data-display/AvatarGroup.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { ImageType } from '$lib/interfaces/image.interface';
+	import Avatar from '$lib/components/data-display/Avatar.svelte';
+
+	export let avatars: ImageType[] = [];
+</script>
+
+<div class="avatar-group -space-x-6">
+	{#each avatars as avatar}
+		<Avatar>
+			<img src={avatar.src} alt={avatar.alt} />
+		</Avatar>
+	{:else}
+		<slot />
+	{/each}
+</div>
